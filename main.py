a913052e961442f8f672c13ec4f0aec5a8b75f11
Andrew Steurer <andrew.w.steurer@gmail.com>
2023-11-09 11:13:29 -0700

    Added summary task feature

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import datetime
 # import os
 import boto3
 from todoist_api_python.api import TodoistAPI
@@ -18,7 +19,8 @@ NUM_MEALS = 1
 try:  
     section_dict = {}
     ingredients_dict = {}
-    recipe_list = []
+    recipe_id_list = []
+    recipe_name_dict = {}
     all_tasks = api.get_tasks()
 
     for project in api.get_projects():
@@ -30,16 +32,19 @@ try:
     for section in api.get_sections():
         section_dict[str.lower(section.name)] = section.id
 
+    # Storing the relevant recipe task data
     for task in all_tasks:
         if not task.is_completed:
             if task.project_id == recipe_id:
                 if task.parent_id == None:
-                    recipe_list.append(task.id)
+                    recipe_id_list.append(task.id)
+                    recipe_name_dict[task.id] = task.content
 
-    # Generating random integers that would correspond with indices in recipe_list
-    recipe_positions = [random.randint(0, len(recipe_list) - 1) for _ in range(NUM_MEALS)]
-    filtered_id_list = [recipe_list[index] for index in recipe_positions ]
+    # Generating random integers that would correspond with indices in recipe_id_list
+    recipe_positions = [random.randint(0, len(recipe_id_list) - 1) for _ in range(0, NUM_MEALS)] # TODO: create a check to be sure that the positional number is unique
+    filtered_id_list = [recipe_id_list[index] for index in recipe_positions]
 
+    # Harmonizing ingredient type and quantity
     for task in all_tasks:
         if not task.is_completed:
             if task.parent_id in filtered_id_list:
@@ -49,6 +54,7 @@ try:
                 else :
                     ingredients_dict[ingredient] = {"quantity": int(quantity), "label": task.labels[0]}
     
+    # Creating the shopping list
     for ingredient in ingredients_dict:
         entry = ingredients_dict[ingredient]
         content = ingredient + ": " + str(entry["quantity"])
@@ -57,6 +63,15 @@ try:
             section_id=section_dict[entry["label"]], 
             project_id=shopping_id
         )
+    
+    # Creating the summary task in the inbox which contains the recipes for the current week
+    recipe_summary_parent_id = api.add_task(content=f'Recipes for the week of {datetime.datetime.now().strftime("%m/%d/%Y")}').id
+
+    for key in recipe_name_dict:
+        if key in filtered_id_list:
+            print(key)
+            api.add_task(content=recipe_name_dict[key], parent_id=recipe_summary_parent_id)
+
 except Exception as error:
     print(error)
 
